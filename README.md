740a3e7fb5bf92521fd995159526d0c9d50ff1d2
André Terron <andrev.terron@gmail.com>
2018-01-27 16:54:35 -0200

    Create firestore live model

---
+++ README.md
@@ -0,0 +1,17 @@
+# Live Model - Firestore
+
+Live Model implementation for Firestore
+
+## Installation
+
+yarn
+
+`yarn add live-model-firestore`
+
+npm
+
+`npm install --save live-model-firestore`
+
+## Licence
+
+[MIT](https://github.com/andreterron/live-model-firestore/blob/master/LICENSE)
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { FirestoreDataManager } from './firestore-data-manager';
+export { FirestoreLiveObject } from './firestore-live-object';
+export { FirestoreSubCollectionLiveList } from './firestore-sub-collection-live-list';
+export { FirestoreManyToManyLiveList } from './firestore-many-to-many-live-list';
+export { FirestoreQueryLiveList } from './firestore-query-live-list';
+export { FirestoreOneToManyLiveList } from './firestore-one-to-many-live-list';
